62839b9780cb257341b775b5f482c0f0c203e31e
Lord-Chaos-YT <thumper540@gmail.com>
2020-10-21 11:21:32 -0700

    Forgot to add a file

--- UI/scene_h.hpp
+++ UI/scene_h.hpp
@@ -16,20 +16,20 @@ namespace UI {
     class Scene : public sf::Drawable {
     public:
         std::function<void(Scene* scene)> onSceneSwitchTo, onSceneSwitchAway, onTick;
-        std::map<std::string, Focus*> focusable;
-        std::map<std::string, Hold*> holdable;
-        std::map<std::string, Hover*> hoverable;
-        std::map<std::string, sf::Drawable*> drawable;
+        std::vector<Focus*> focusable;
+        std::vector<Hold*> holdable;
+        std::vector<Hover*> hoverable;
+        std::vector<sf::Drawable*> drawable;
     private:
         virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
-            for (auto val : drawable) target.draw(*val.second, states);
+            for (auto val : drawable) target.draw(*val, states);
         }
     public:
         Scene(
-            std::map<std::string, Focus*> focusVect = {},
-            std::map<std::string, Hold*> holdVect = {},
-            std::map<std::string, Hover*> hoverVect = {},
-            std::map<std::string, sf::Drawable*> drawVect = {}
+            std::vector<Focus*> focusVect = {},
+            std::vector<Hold*> holdVect = {},
+            std::vector<Hover*> hoverVect = {},
+            std::vector<sf::Drawable*> drawVect = {}
         );
 
         void setSwitchToAction(std::function<void(Scene* scene)> fnc);
