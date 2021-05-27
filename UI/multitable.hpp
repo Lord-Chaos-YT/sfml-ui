8255f7ae8ba3a65bacdd53fde346cb5ff2877440
Lord-Chaos-YT <thumper540@gmail.com>
2021-05-27 12:58:21 -0600

    Added table functionality

--- UI/multitable.hpp
+++ UI/multitable.hpp
@@ -1,14 +1,16 @@
-#ifndef UI_TABLE_HPP
-#define UI_TABLE_HPP
+#ifndef UI_MULTITABLE_HPP
+#define UI_MULTITABLE_HPP
 
 #include <SFML/Graphics.hpp>
-#include "elements.hpp"
-#include "table_h.hpp"
+#include "multielements.hpp"
+#include "concrete_h.hpp"
+#include "background.hpp"
+#include "multitable_h.hpp"
 
 namespace UI {
     template<class BackgroundType> Table<BackgroundType>::Table(
         const ArrayType& initElems, const sf::Vector2f& size, const sf::Vector2f& innerPad, const sf::Vector2f& outerPad
-    ) : elements{initElems}, innerPadding{innerPad}, outerPadding{outerPad} {
+    ) : elements{initElems}, defDim{defD}, innerPadding{innerPad}, outerPadding{outerPad} {
         if (!std::is_base_of<sf::RectangleType, BackgroundType>::value) throw "Invalid type specifier. Type must inherit from class sf::RectangleShape!";
         this->recomputeSizes();
     }
@@ -16,14 +18,17 @@ namespace UI {
     template<class BackgroundType> void Table<BackgroundType>::recomputePos() {
         sf::Vector2f pos = background.getPosition() + outerPadding;
         for (std::size_t i = 0u; i < elements.size(); i++)
-            for (std::size_t j = 0u; j < elements[i].size(); j++)
-                elements[i][j].setPosition(pos + {(calcedElementSize.x * (float)(i - 1u)) + (innerPadding.x * (float)(i - 1u)), (calcedElementSize.y * (float)(j - 1u)) + (innerPadding.y * (float)(j - 1u))});
+            for (std::size_t j = 0u, g = 0u; j < elements[i].size(); j++, g += elements[i][j].getGridSpace().y)
+                elements[i][j].setPosition(pos + {(calcedElementSize.x * (float)i) + ((float)(i - 1u) * innerPadding.x), (calcedElementSize.y * (float)g) + ((float)(g - 1u) * innerPadding.y)});
     }
 
     template<class BackgroundType> void Table<BackgroundType>::recomputeSizes() {
         calcedGridSize = {elements.size(), 0u};
-        for (auto& column : elements) if (column.size() > calcedGridSize.y) calcedGridSize.y = column.size();
-
+        for (auto& column : elements) {
+            std::size_t temp = 0u;
+            for (ElementType& element : column) temp += element.getGridSpace().y;
+            if (temp > calcedGridSize.y) calcedGridSize.y = temp;
+        }
         calcedElementSize = background.getSize() - {(innerPadding.x * (float)(calcedGridSize.x - 1u)) + (outerPadding.x * 2.f), (innerPadding.y * (float)(calcedGridSize.y - 1u)) + (outerPadding.y * 2.f)};
         calcedElementSize.x = calcedElementSize.x / calcedGridSize.x;
         calcedElementSize.y = calcedElementSize.y / calcedGridSize.y;
@@ -57,22 +62,27 @@ namespace UI {
     }
 
     template<class BackgroundType> void Table<BackgroundType>::erase(const sf::Vector2u& place) {
-        if (!place.y && elements.at(place.x).size() == 1) elements.erase(place.x);
+        if (!place.y && [place.x].size() == 1) elements.erase(place.x);
         else elements[place.x].erase(place.y);
         this->recomputeSizes();
     }
 
     template<class BackgroundType> void Table<BackgroundType>::replaceElement(const ElementType& replaced, sf::Vector2i place) {
         if (place.x == -1) place.x = elements.size() - 1;
-        if (place.y == -1) place.y = elements.at(place.x).size() - 1;
+        if (place.y == -1) place.y = elements.at(0).size() - 1;
+        sf::Vector2u gridSpaceCurrent = elements.at(place.x).at(place.y).getGridSpace(), gridSpaceReplaced = replaced.getGridSpace();
         elements[place.x][place.y] = replaced;
+        if (gridSpaceCurrent.x < gridSpaceReplaced.x) {
+            if (place.x + gridSpaceReplaced.x < elements.size()) ;
+        }
+        this->recomputeSizes();
     }
 
     template<class BackgroundType> const std::vector<const std::vector<const Table<BackgroundType>::ElementType>>& Table<BackgroundType>::getElements() const {return elements;}
     template<class BackgroundType> sf::Vector2f Table<BackgroundType>::getPosition() const {return background.getPosition();}
     template<class BackgroundType> sf::Vector2f Table<BackgroundType>::getSize() const {return background.getSize();}
     
-    template<class BackgroundType> sf::Vector2u Table<BackgroundType>::getTableSize() const {return calcedGridSize;}
+    template<class BackgroundType> sf::Vector2u Table<BackgroundType>::getTableSize() const {return calcedGridDim;}
 };
 
 #endif
--- UI/multitable_h.hpp
+++ UI/multitable_h.hpp
@@ -1,26 +1,34 @@
-#ifndef UI_TABLE_H_HPP
-#define UI_TABLE_H_HPP
+#define SFML_STATIC
+
+#ifndef UI_MULTITABLE_H_HPP
+#define UI_MULTITABLE_H_HPP
 
 #include <SFML/Graphics.hpp>
-#include "elements.hpp"
+#include <multielements_h.hpp>
+#include "concrete.hpp"
+#include "methods.hpp"
+#include <iostream>
+#include <string>
 
 namespace UI {
-    template<class BackgroundType> class Table : public sf::Drawable {
+    template<class BackgroundType = sf::RectangleShape> class Table : public Concrete {
     public:
-        typedef Element<BackgroundType> ElementType;
+        typedef MultiElement<BackgroundType> ElementType;
         typedef std::vector<std::vector<ElementType>> ArrayType;
     private:
+        BackgroundType background;
         ArrayType elements;
         sf::Vector2f innerPadding, outerPadding, calcedElementSize;
         sf::Vector2u calcedGridSize;
-        bool autoResize = false;
-
         virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+            target.draw(background, states);
+            for (std::vector<ElementType>& column : elements) for (ElementType& elem : column) target.draw(elem, states);
         }
 
         void recomputePos();
         void recomputeSizes();
     public:
+
         Table(const ArrayType& initElems = {}, const sf::Vector2f& size = {0.f, 0.f}, const sf::Vector2f& innerPad = {4.f, 4.f}, const sf::Vector2f& outerPad = {4.f, 4.f});
 
         void setPosition(const sf::Vector2f& pos);
